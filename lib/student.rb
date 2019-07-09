dfdaf5897d35f64d1ef5b56ab2b409f8ba6976cc
Sean Hetzel <seanhetzel1@gmail.com>
2019-07-09 20:32:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,10 +66,13 @@ class Student
     sql = <<-SQL
     SELECT name
     FROM students
-    WHERE name = #{name}
+    WHERE name = ?
+    LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
